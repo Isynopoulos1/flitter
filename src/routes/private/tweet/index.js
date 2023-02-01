c2d39a1c3398b0393f25141482f9c4a7a3358a7f
Isela Alarcón <isela.v.alarcon@gmail.com>
2023-02-01 18:34:47 +0100

    adding tweets endpoint structure

--- src/routes/private/tweet/index.js
+++ src/routes/private/tweet/index.js
@@ -12,4 +12,18 @@ router.post("/api/tweet", isAuth, async (req, res) => {
   // TODO crate tweet
 })
 
+// @route  PUT api/tweet
+// @desc   Modify an existing tweet as a user
+// @access Private
+router.put("/api/tweet/:tweetId", isAuth, async (req, res) => {
+  // TODO crate tweet
+})
+
+// @route  DELETE api/tweet
+// @desc   delete a tweet as a user
+// @access Private
+router.delete("/api/tweet/:tweetId", isAuth, async (req, res) => {
+  // TODO crate tweet
+})
+
 module.exports = router
