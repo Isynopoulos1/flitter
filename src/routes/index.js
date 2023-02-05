10015b663c0eaa5de08b18593f7e6ef33ab2e931
unknown <elisabet.v.alvarez@gmail.com>
2023-02-05 12:45:46 +0100

    log out added

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,6 @@
 const register = require("./public/register")
 const login = require("./public/login")
+const logout = require("./public/logout")  
 const user = require("./private/user")
 
-module.exports = routes = [register, login, user]
+module.exports = routes = [register, login, , logout, user]
